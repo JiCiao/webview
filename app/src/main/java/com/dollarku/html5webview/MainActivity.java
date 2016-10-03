d160b018a83da6105d4f6744869afbe46007e578
jcluo <jcluo@potentia.asia>
2016-10-03 16:14:35 +0800

    first commit

---
+++ app/src/main/java/com/dollarku/html5webview/MainActivity.java
@@ -0,0 +1,57 @@
+package com.dollarku.html5webview;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String URL = "http://daxiaguilai.games.wakool.tw/";
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupWebView();
+    }
+
+    private void setupWebView(){
+        webView = (WebView)findViewById(R.id.web);
+
+        webView.setWebViewClient(webViewClient);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.loadUrl(URL);
+    }
+
+    private WebViewClient webViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.toString());
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+        }
+    };
+}
